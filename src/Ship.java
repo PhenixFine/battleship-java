e065fcf372612733b35ddcb32c136d9c694f134b
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-06-14 15:07:25 -0400

    Stage 4 of 5 for JetBrains Academy - Java - Battleship project. Ship.java changes: • I have three of the new methods throw an exception if they aren't used properly from whatever called it, but handled properly the methods will not throw an exception ( I wanted an exception to be thrown if I didn't handle them properly in the new Class Battlefield ). • added HashMap coordinates to keep track of the ship's coordinates, and remove a coordinate when the ship is hit. • addCoordinate() - method handles adding a coordinate for the ship, throwing an exception if called more times than the size of the ship. • hitShip() - removes a coordinate when the ship is hit, throwing an exception if an attempt to hit the ship is made when it is already sunk. It receives two numbers and a boolean vertical, indicating if the ship is positioned vertically. • isSunk() - returns true when all coordinates have been removed from the HashMap coordinates. • findShip() - a static method that checks which ship from a given list is positioned at the given coordinates ( and it received a boolean vertical that indicates whether the ship is positioned vertically to help find the ship ).

--- src/Ship.java
+++ src/Ship.java
@@ -1,9 +1,40 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class Ship {
     final String name;
     final int size;
+    private final HashMap<Integer, Integer> coordinates = new HashMap<>();
 
     public Ship(String name, int size) {
         this.name = name;
         this.size = size;
+    }
+
+    public void addCoordinate(int i, int j) {
+        if (coordinates.size() < size) {
+            coordinates.put(i, j);
+        } else throw new RuntimeException("Ship size exceeded.");
+    }
+
+    public void hitShip(int i, int j, boolean vertical) {
+        if (coordinates.size() != 0) {
+            coordinates.remove(vertical ? i : j, vertical ? j : i);
+        } else throw new RuntimeException("Ship is already sunk.");
+    }
+
+    public boolean isSunk() {
+        return coordinates.isEmpty();
+    }
+
+    public static Ship findShip(int i, int j, List<Ship> ships, boolean vertical) {
+        for (Ship ship : ships) {
+            if (ship.coordinates.containsKey(vertical ? i : j)) {
+                if (ship.coordinates.get(vertical ? i : j).equals(vertical ? j : i)) {
+                    return ship;
+                }
+            }
+        }
+        throw new RuntimeException("There was no ship.");
     }
 }
